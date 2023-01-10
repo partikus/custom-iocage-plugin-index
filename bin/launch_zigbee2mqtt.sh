5e1d1a539f6591c1b1270ab0d373c57a543feb52
Michal Kruczek <partikus@users.noreply.github.com>
2023-01-10 16:01:29 +0100

    feat: zigbee2mqtt - fix

--- bin/launch_zigbee2mqtt.sh
+++ bin/launch_zigbee2mqtt.sh
@@ -7,7 +7,7 @@ name=${1:-"z2m"}
 # absolute path of the folder containng the current script
 script_folder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 templates_folder=$(realpath "${script_folder}/../templates")
-output_folder=$(realpath "/mnt/$(iocage get -p)/bin/")
+output_folder=$(realpath "/mnt/$(iocage get -p)/iocage/bin/")
 prestart_script_path="${output_folder}/${name}_prestart.sh"
 poststart_script_path="${output_folder}/${name}_poststart.sh"
 
